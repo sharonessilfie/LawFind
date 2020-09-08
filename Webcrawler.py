c0851dbf6097c6991808b9aa08a9c60466712751
Sharon Essilfie <68645081+sharonessilfie@users.noreply.github.com>
2020-09-08 16:53:21 +0000

    Add files via upload

---
+++ Webcrawler.py
@@ -0,0 +1,14 @@
+
+import re
+
+class PyCrawler(object):
+    def __init__(self, starting_url):
+        self.starting_url = starting_url
+        self.visited = set()
+
+    def start(self):
+        pass
+
+if __name__ == "__main__":
+    crawler = PyCrawler()
+    crwaler.start()
